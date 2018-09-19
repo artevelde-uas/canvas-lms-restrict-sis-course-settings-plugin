1cea40baf5104f3b418249f9698f27fd80d02a90
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2018-09-19 11:22:17 +0200

    Disable 'SIS ID', 'Sub-account' and 'Term' inputs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import disableCourseActions from './plugins/disableCourseActions';
+import disableCourseSettings from './plugins/disableCourseSettings';
 import { getCourse } from './api';
 
 
 export default function (canvas) {
     canvas.addRouteListener('course.settings', function (params) {
         
-        getCourse(params.courseId, disableCourseActions);
+        getCourse(params.courseId, function (course) {
+            disableCourseActions(course);
+            disableCourseSettings(course);
+        });
         
     });
 }
